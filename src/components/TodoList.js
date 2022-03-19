394753239ff3ee1694109c9176ac6ae3513e067e
Bitna Lee <erin@ERIN.local>
2022-03-19 18:42:52 +0900

    React todoList

---
+++ src/components/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import TodoListItem from './TodoListItem';
+
+const TodoList = ({ todos, onRemove, onToggle }) => {
+  return (
+    <div className="TodoList">
+      {todos.map((todo) => (
+        <TodoListItem
+          todo={todo}
+          onRemove={onRemove}
+          onToggle={onToggle}
+          key={todo.id}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
---
+++ src/components/TodoListItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
+import './TodoListItem.scss';
+import cn from 'classnames';
+
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+  const { id, text, checked } = todo;
+  const onClick = () => {
+    onToggle(id);
+    console.log(id);
+  };
+  return (
+    <div className="TodoListItem">
+      <div onClick={onClick} className={cn('checkbox', { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
+      </div>
+      <div className="remove" onClick={() => onRemove(id)}>
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
+  );
+};
+
+export default TodoListItem;
